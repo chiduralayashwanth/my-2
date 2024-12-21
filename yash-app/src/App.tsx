7f0bc8c13c21b5153ddff3367764879d9affc0d6
chiduralayashwanth <22bk1a7228@stpetershyd.com>
2024-12-21 09:43:57 +0530

    first commit

---
+++ yash-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from './Components/NavBar'
+import Home from './Components/Home'
+import Movies from './Components/Movies'
+
+
+
+export default function App() {
+  return (
+    <div>
+      
+      
+      <Movies/>
+      </div>
+  )
+}
